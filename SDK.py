22fda52736b83132ceb3517a05761a226d1b9eeb
Nikita <Kent720p@gmail.com>
2021-06-13 20:52:15 +0300

    created logic for customers: create and update

--- SDK.py
+++ SDK.py
@@ -1,47 +1,57 @@
 import sqlite3
 from customer import Customer
 
+
 def cursor():
-	return sqlite3.connect('data.db').cursor()
+    return sqlite3.connect('data.db').cursor()
 
 
 def add_customer(customer):
-	c = cursor()
+    c = cursor()
 
-	with c.connection:
-		c.execute('INSERT INTO customers VALUES (?, ?, ?)', (customer.name, customer.surname, customer.address))
-	c.connection.close()	
-	return c.lastrowid
+    with c.connection:
+        c.execute('INSERT INTO customers VALUES (?, ?, ?)', (customer.name, customer.surname, customer.address))
+    c.connection.close()
+    return c.lastrowid
+
 
 def get_customers():
-	c = cursor()
-	customers = []
+    c = cursor()
+    customers = []
 
-	with c.connection:
-		c.execute('SELECT * FROM customers')
-		records = c.fetchall()
+    with c.connection:
+        c.execute('SELECT * FROM customers')
+        records = c.fetchall()
 
-		for row in records:
-			print(f'{row[0]} {row[1]} {row[2]}')
+        for row in records:
+            print(f'{row[0]} {row[1]} {row[2]}')
 
-	c.connection.close()
+    c.connection.close()
+
 
 def get_customer_by_name(name):
-	c = cursor()
-	with c.connection:
-		c.execute('SELECT * FROM customers WHERE name=?', (name,))
-	data = c.fetchone()
+    c = cursor()
+    with c.connection:
+        c.execute('SELECT * FROM customers WHERE name=?', (name,))
+    data = c.fetchone()
 
-	if not data:
-		return None
-	
-	return Customer(data[0],data[1],data[2])
+    if not data:
+        return None
+
+    return Customer(data[0], data[1], data[2])
+
 
 def update_customer(customer, new_name, new_surname, new_address):
+    c = cursor()
+    with c.connection:
+        c.execute('''UPDATE customers SET name=?, surname=?,
+		  		 address=? WHERE name=? AND surname=? AND address=?''',
+                  (new_name, new_surname, new_address, customer.name, customer.surname, customer.address))
+    customer = get_customer_by_name(new_name)
+    c.connection.close()
+    return customer
+
+def change(customer_name):
 	c = cursor()
 	with c.connection:
-		c.execute('''UPDATE customers SET name=?, surname=?,
-		  		 address=? WHERE name=? AND surname=? AND address=?''', (new_name, new_surname, new_address, customer.name, customer.surname, customer.address))
-	customer = get_customer_by_name(new_name)
-	c.connection.close()	
-	return customer
+		c.execute(''' ''')
--- index.py
+++ index.py
@@ -1,7 +1,49 @@
 from customer import Customer
 import SDK
 
-customer = Customer("dagestan", "priorov", "moskov")
+# customer = Customer("dagestan", "priorov", "moskov")
 
-print(SDK.get_customer_by_name("NEWNAME"))
-print(SDK.update_customer(customer, "Vasya"))
+# print(SDK.get_customer_by_name("NEWNAME"))
+# print(SDK.update_customer(customer, "Vasya"))
+
+while True:
+    print('''
+    1. информация о покупателях
+    2. 
+    3.
+    4.
+    ''')
+
+    user_input = int(input())
+
+    if user_input == 1:
+        while True:
+            print(SDK.get_customers())
+
+            print('''
+            1. изменить покупателя
+            2. добавить покупателя
+            
+            0. выход
+            ''')
+
+            user_input = int(input())
+
+            if user_input == 1:
+                while True:
+                    print(SDK.get_customers())
+                    customer_name = input('введите имя покупателя \n')
+                    customer_obj = SDK.get_customer_by_name(customer_name)
+
+                    user_input = int(input('изменить покупателя? \n 1. да \n 2. нет \n'))
+
+                    if user_input == 1:
+                        customer_new_name = input('введите новое имя: \n')
+                        customer_new_surname = input('введите новую фамилию: \n')
+                        customer_new_address = input('введите новый адрес: \n')
+                        print(SDK.update_customer(customer_obj, customer_new_name, customer_new_surname, customer_new_address))
+
+                    if user_input == 2:
+                        break
+            else:
+                break
